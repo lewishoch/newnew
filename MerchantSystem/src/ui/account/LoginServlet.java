d8f6c3a0dd8e6dbf5dce1eee377ac14c4a45b39a
hwong443 <hwong443@gmail.com>
2016-10-14 16:38:45 +0800

    merchant 0.5

--- MerchantSystem/src/ui/account/LoginServlet.java
+++ MerchantSystem/src/ui/account/LoginServlet.java
@@ -8,11 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import po.Merchant;
+import service.MerchantManager;
+import service.impl.MerchantManagerImpl;
+
 /**
  * Servlet implementation class LoginServlet
  */
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private MerchantManager mm = new MerchantManagerImpl();
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -22,16 +27,17 @@ public class LoginServlet extends HttpServlet {
 		String uname = (String)request.getParameter("uname");
 		String password = (String)request.getParameter("password");
 		
+		Merchant merchant = mm.loadMerchant(uname);
+		
 		// check if valid
-		if(true){
-			sen.setAttribute("uname", uname);
-			sen.setAttribute("password", password);
+		if(merchant!=null && merchant.getPsd().equals(password)){
+			sen.setAttribute("id", merchant.getUuid());
 			request.getRequestDispatcher("control.jsp").forward(request,response);
 		}
 		else{
 			// else
-			
+			request.setAttribute("loginMsg", "Wrong user name or password.");
+			request.getRequestDispatcher("index.jsp").forward(request,response);
 		}
 	}
-
 }
