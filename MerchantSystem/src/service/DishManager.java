41a656aae92d8281dfdd1d1207997f373ad00b24
Clara Lo <claralo7718@gmail.com>
2016-10-16 01:57:07 +0800

    add findDishesByMerchantId()

--- MerchantSystem/src/service/DishManager.java
+++ MerchantSystem/src/service/DishManager.java
@@ -6,8 +6,10 @@ import po.Dish;
 
 public interface DishManager {
 	public Dish loadDish(long dishUuid);
+	public Dish loadDish(String dishName, long merchantId);
 	public boolean addDish(Dish dish);
 	public boolean updateDish(Dish dish);
 	public boolean deleteDish(long dishUuid);
-	public List<Dish> findAllDishes(long merchantUuid);
+	public List<Dish> findAllDishes();
+	public List<Dish> findDishesByMerchantUuid(long merchantUuid);
 }
