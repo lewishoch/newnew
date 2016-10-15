7fe1d99d9d83a9cbdb1f1aaaba8adf03ce9a68d3
Stevie <hwong443@gmail.com>
2016-10-16 00:07:48 +0800

    fix2

--- MerchantSystem/src/ui/common/SessionLogin.java
+++ MerchantSystem/src/ui/common/SessionLogin.java
@@ -15,7 +15,13 @@ public class SessionLogin {
 		
 		try{
 			// check in case is null;
-			return true ;
+			System.out.println("isLogin: "+sen.getAttribute("isLogin"));
+			System.out.println("uuid: "+sen.getAttribute("uuid"));
+			boolean isLogin = (boolean)sen.getAttribute("isLogin");
+			//long uuid = Long.parseLong((String) sen.getAttribute("uuid"));
+			System.out.println("isLogin: "+sen.getAttribute("isLogin"));
+			//System.out.println("uuid: "+uuid);
+			return isLogin ;
 		}
 		catch(Exception e){
 			System.out.println("fail");
